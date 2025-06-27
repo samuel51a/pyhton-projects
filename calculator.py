3dac00ac2851b86244fb45ca77ac5103d527c782
samuel51a <samuelkhronik@gmail.com>
2025-06-27 15:45:27 +0200

    modified my calculation functions for better results

--- calculator.py
+++ calculator.py
@@ -1,19 +1,27 @@
-num1=input("Enter first number :")
-num2=input("Enter second number :")
-operation=input("Choose operation (+,-,*,/) :")
-try:
-    num1=float(num1)
-    num2=float(num2)
+def calculation(num1,num2,operation):
+        try:
+            num1=float(num1)
+            num2=float(num2)
 
-    if operation=='+':
-        print(int(num1)+int(num2))
-    elif operation=='-':
-        print(int(num1)-int(num2))
-    elif operation=='*':
-        print(int(num1)*int(num2))
-    elif operation=='/':
-        print(int(num1)/int(num2))
-    else:
-        print("Please enter valid operation")
-except ValueError:
-    print("Enter a valid number.")
+            if operation=='+':
+                return num1+num2
+            elif operation=='-':
+                return num1-num2
+            elif operation=='*':
+                return num1*num2
+            elif operation=='/':
+                return num1/num2
+            else:   
+                return "Please enter valid operation"
+        except ValueError:
+            return "Enter a valid number."
+
+def main():
+
+    num1=input("Enter first number :")
+    num2=input("Enter second number :")
+    operation=input("Choose operation (+,-,*,/) :")
+    results=calculation(num1,num2,operation)
+    print(results)
+if __name__ == "__main__":
+    main()
